fef37f0b3c637afbe3c57775eff91d40a55d5d61
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-03 13:19:19 +0900

    set up next-auth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -14,7 +14,7 @@ import {
 function Sidebar() {
   return (
     <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
-      <div>
+      <div className='space-y-4'>
         <button className='flex items-center space-x-2 hover:text-white'>
           <HomeIcon className='h-5 w-5'/>
           <p>Home</p>
@@ -43,6 +43,12 @@ function Sidebar() {
           <p>Your episodes</p>
         </button>
         <hr className='border-t-[0.1px] border-gray-900'></hr>
+
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
       </div>
     </div>
 
