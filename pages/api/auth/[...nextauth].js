859dd1f8e8456b60d8e2c9dc92d54234624a0a5c
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-03 13:58:34 +0900

    set refresh token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,27 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { refreshAccessToken } from "spotify-web-api-node/src/server-methods";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+async function refreshAccessToken(token){
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +33,32 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt({ token, account, user }){
+      if (account && user){
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+      if (Date.now() < token.accessTokenExpires){
+        console.log('TOKEN IS UP TO DATE');
+        return token;
+      }
+      console.log("NEED TO REFRESH");
+      return await refreshAccessToken(token);
+    },
+    async session({ session, token }){
+      session.user.accessToken = token.setAccessToken;
+      session.user.refreshToken = token.accessToken;
+      session.user.username = token.username;
+    }
+  },
 })
